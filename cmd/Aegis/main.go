5bc9d6008f1a5a37a9a4ad60ca1e5e650a3b06f1
Niiaks <napappoe007@st.ug.edu.gh>
2026-01-13 21:14:13 +0000

    refactor: implement database and logger packages with New Relic integration

--- cmd/Aegis/main.go
+++ cmd/Aegis/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
-	fmt.Println("welocome to aegis")
+ var wg sync.WaitGroup
+
+ for i := 0; i < 3; i++ {
+  defer fmt.Println("defer:", i)
+
+  wg.Add(1)
+  go func(n int) {
+   defer wg.Done()
+   fmt.Println("goroutine:", n)
+  }(i)
+ }
+
+ wg.Wait()
 }
