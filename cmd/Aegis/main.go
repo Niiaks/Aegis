12a7ad3c819f216533d080baf26a7796eb4043f0
Niiaks <napappoe007@st.ug.edu.gh>
2026-01-16 23:53:44 +0000

    remove goroutine logic

--- cmd/Aegis/main.go
+++ cmd/Aegis/main.go
@@ -2,21 +2,8 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
 
 func main() {
- var wg sync.WaitGroup
-
- for i := 0; i < 3; i++ {
-  defer fmt.Println("defer:", i)
-
-  wg.Add(1)
-  go func(n int) {
-   defer wg.Done()
-   fmt.Println("goroutine:", n)
-  }(i)
- }
-
- wg.Wait()
+	fmt.Println("Aegis ")
 }
