8a56d7002c64374c5ab2d3f073d8ecd2ab09a981
Niiaks <napappoe007@st.ug.edu.gh>
2026-01-24 21:38:21 +0000

    feat: add WalletType and constants for AccountExternalID and AccountPlatformID

---
+++ pkg/constants/constants.go
@@ -0,0 +1,8 @@
+package constants
+
+type WalletType string
+
+const (
+	AccountExternalID WalletType = "00000000-0000-0000-0000-000000000001"
+	AccountPlatformID WalletType = "00000000-0000-0000-0000-000000000002"
+)
